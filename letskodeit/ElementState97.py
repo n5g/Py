f18a26e122ff1a4486961f424bfcc625ff8f425e
n5g <55139148+n5g@users.noreply.github.com>
2020-03-08 19:16:10 +0500

    Update ElementState97.py

--- letskodeit/ElementState97.py
+++ letskodeit/ElementState97.py
@@ -5,7 +5,7 @@ driver.maximize_window()
 driver.implicitly_wait(5)
 driver.get("https://www.google.com")
 e1 = driver.find_element_by_name("q")
-e1State = e1.is_enabled()  #узнать доступен ли элемент?
+e1State = e1.is_enabled()   # узнать доступен ли элемент?
 print("E1 is Enabled? -> " +str(e1State))
 e1.send_keys("letskodeit")
 time.sleep(3)
