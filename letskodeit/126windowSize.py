b4dbe19f22363e8d5a42dd6dffaaef20d934c06d
n5g <55139148+n5g@users.noreply.github.com>
2020-03-09 22:13:06 +0500

    Update 126windowSize.py

--- letskodeit/126windowSize.py
+++ letskodeit/126windowSize.py
@@ -10,7 +10,7 @@ class Screenshots():
 
         height = driver.execute_script("return window.innerHeight;")
         width = driver.execute_script("return window.innerWidth;")
-        print("Height: " + str(height))
+        print("Height:  " + str(height))
         print("Width: " + str(width))
         driver.quit()
 
